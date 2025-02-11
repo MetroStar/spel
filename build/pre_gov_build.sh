09a5f2478cafb9becf889cc9109ca5d7df596418
padili-metrostar <padili@metrostar.com>
2025-02-11 15:45:10 +0000

    Add pre-build and build scripts for GovCloud AMI management

---
+++ build/pre_gov_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu -o pipefail
+
+echo "==========STARTING PRE_BUILD=========="
+echo "Installing required plugins in packer template, spel/minimal-linux.pkr.hcl"
+
+packer init spel/minimal-linux.pkr.hcl
+
+echo "Validating packer template, spel/minimal-linux.pkr.hcl"
+
+packer validate \
+    -only "${SPEL_BUILDERS:?}" \
+    -var "spel_identifier=${SPEL_IDENTIFIER:?}" \
+    -var "spel_version=${SPEL_VERSION:?}" \
+    -var "aws_region=us-gov-west-1" \
+    spel/minimal-linux.pkr.hcl
