104486bb4b9e0116047145234d5e754500dd9e08
padili-metrostar <padili@metrostar.com>
2025-06-11 17:52:01 +0000

    Refactor BuildChroot function to invoke disk-partitioner before hardening the AMI

--- spel/scripts/amigen9-build.sh
+++ spel/scripts/amigen9-build.sh
@@ -175,16 +175,16 @@ function BuildChroot {
     # Prepare the build device
     PrepBuildDevice
 
+    # Invoke disk-partitioner
+    bash -euxo pipefail "${ELBUILD}"/$( ComposeDiskSetupString ) || \
+        err_exit "Failure encountered with DiskSetup.sh"
+
     # Harden the AMI
     python3 -m pip install ansible
     export PATH="/usr/local/bin:$PATH"
     which ansible-playbook
     git clone --depth=1 -b devel https://github.com/ansible-lockdown/RHEL9-STIG.git
     cd RHEL9-STIG && ansible-playbook -i localhost, -c local site.yml -e "system_is_ec2=true"
-
-    # Invoke disk-partitioner
-    bash -euxo pipefail "${ELBUILD}"/$( ComposeDiskSetupString ) || \
-        err_exit "Failure encountered with DiskSetup.sh"
 
     # Invoke chroot-env disk-mounter
     bash -euxo pipefail "${ELBUILD}"/$( ComposeChrootMountString ) || \
