15b77cbc221d320b623657bc8f5321e77ee7761e
padili-metrostar <padili@metrostar.com>
2025-06-16 15:00:19 +0000

    Update ansible-playbook command to change audit output destination

--- spel/scripts/amigen9-build.sh
+++ spel/scripts/amigen9-build.sh
@@ -223,7 +223,7 @@ function BuildChroot {
     python3 -m pip install ansible
     export PATH="/usr/local/bin:$PATH"
     git clone --depth=1 -b devel https://github.com/ansible-lockdown/RHEL9-STIG.git
-    ansible-playbook -i localhost, -c local RHEL9-STIG/site.yml -e '{"system_is_ec2": true, "setup_audit": true, "run_audit": true, "audit_output_collection_method": "copy", "audit_output_destination": "/home/maintuser/audit_output"}'
+    ansible-playbook -i localhost, -c local RHEL9-STIG/site.yml -e '{"system_is_ec2": true, "setup_audit": true, "run_audit": true, "audit_output_collection_method": "copy", "audit_output_destination": "/opt/hardening/audit/"}'
 
     # Invoke unmounter
     bash -euxo pipefail "${ELBUILD}"/Umount.sh -c "${AMIGENCHROOT}" || \
