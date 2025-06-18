6e41e6edd3b2769a72ae48169dc64de501a29321
padili-metrostar <padili@metrostar.com>
2025-06-18 20:11:29 +0000

    Remove hardening steps for AMI from BuildChroot function in amigen9-build.sh

--- spel/scripts/amigen9-build.sh
+++ spel/scripts/amigen9-build.sh
@@ -222,12 +222,6 @@ function BuildChroot {
     # Invoke unmounter
     bash -euxo pipefail "${ELBUILD}"/Umount.sh -c "${AMIGENCHROOT}" || \
         err_exit "Failure encountered with Umount.sh"
-
-    # Harden the AMI
-    # python3 -m pip install ansible
-    # export PATH="/usr/local/bin:$PATH"
-    # git clone --depth=1 -b devel https://github.com/ansible-lockdown/RHEL9-STIG.git
-    # ansible-playbook -i localhost, -c local RHEL9-STIG/site.yml -e '{"system_is_ec2": true, "setup_audit": true, "run_audit": true, "fetch_audit_output": true}'
 }
 
 # Create a record of the build
